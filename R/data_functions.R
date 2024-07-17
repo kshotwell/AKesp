4789e94e849e0078fff015beef9788084cb2b967
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2024-07-16 20:35:49 -0400

    allow user to pull all indicator data

--- R/data_functions.R
+++ R/data_functions.R
@@ -1,17 +1,20 @@
 #' Pull ESP data
 #'
 #' This function pulls ESP data on a specific stock from the AKFIN web service.
-#' @param stock The name of the stock to pull data for. Use `AKesp::esp_stock_options()` to view current options.
+#' @param stock The name of the stock to pull data for. Use `AKesp::esp_stock_options()` to view current options. Leave as NULL to get all data.
 #' @return A tibble
 #' @importFrom magrittr %>%
 #' @export
 
-get_esp_data <- function(stock) {
-  base_url <- "https://apex.psmfc.org/akfin/data_marts/akmp/esp_indicators?intended_esp="
-  stock_url <- stock %>%
-    stringr::str_replace_all(" ", "%20")
+get_esp_data <- function(stock = NULL) {
+  base_url <- "https://apex.psmfc.org/akfin/data_marts/akmp/esp_indicators?"
 
-  url <- paste0(base_url, stock_url)
+  url <- ifelse(is.null(stock),
+                base_url,
+                paste0(base_url,
+                       "intended_esp=",
+                       stock %>%
+                         stringr::str_replace_all(" ", "%20")))
 
   data <- httr::content(httr::GET(url),
     type = "application/json"
